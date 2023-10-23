13e7d414319f40e77c3c6215c6bc747c19798c98
CreeJee <creejee@quotabook.com>
2023-10-24 00:25:18 +0900

    fix: 메인 추가세팅

--- services/kirby-motion-app/README.md
+++
@@ -1,7 +0,0 @@
-
-## Getting Started
-개발 서버를 여는 방법
-
-1. kirby-motion-app 에서 .env.dev 의 파일을 만든다
-2. `yarn dev` 로 서버를 연다
-3. [http://localhost:3000](http://localhost:3000) 을 연다
--- services/kirby-motion-app/src/app/page.tsx
+++ services/kirby-motion-app/src/app/page.tsx
@@ -1,13 +1,5 @@
 import { Index } from '@kirby-motion/ui';
 
-import styles from './page.module.css';
-
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div>
-        <Index />
-      </div>
-    </main>
-  );
+  return <Index />;
 }
